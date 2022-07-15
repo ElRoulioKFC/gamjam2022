e742a5a31b7410073cad009c33355746c3690fbf
copsfuraxx <jules.finck@hotmail.fr>
2022-07-15 19:49:49 +0200

    playerscript

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    private int vie;
+    public int Vie { get { return vie; } set { vie = value; } }
+    public int vieMax;
+    public List<Dice> pot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
